6640b59806182d8f21e8d04eb3cf1f00f01fa506
Felpo0132 <felipesiilvaa2006@gmail.com>
2026-07-09 16:08:04 -0300

    Criar registro de ganho
    Mesma dieia mas para ganhos

--- finance/urls.py
+++ finance/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('registrar-gasto/', views.registrar_gasto, name='registrar_gasto'),
+    path('registrar-ganho/', views.registrar_ganho, name='registrar_ganho'),
 ]
--- finance/views.py
+++ finance/views.py
@@ -55,7 +55,7 @@ def dashboard(request):
     total_ganhos = sum_by_value(ganhos)
     saldo_atual = (float)(total_ganhos)-(float)(total_gastos)
 
-    user_context = {"gastos":gastos.order_by('-id')[:5], "ganhos":ganhos.order_by('-id')[:5], "total_gastos":total_gastos, "total_ganho":total_ganhos, "saldo_atual":saldo_atual, "tipos_gastos":tipos_gastos, "tipos_ganhos":tipos_ganhos}
+    user_context = {"gastos":gastos.order_by('-id')[:5], "ganhos":ganhos.order_by('-id')[:5], "total_gastos":total_gastos, "total_ganhos":total_ganhos, "saldo_atual":saldo_atual, "tipos_gastos":tipos_gastos, "tipos_ganhos":tipos_ganhos}
 
     return render(request, "finance/dashboard.html", context=user_context)
 
@@ -72,5 +72,21 @@ def registrar_gasto(request):
     type_id = request.POST.get('type') # Do html recebe so o id, ai o Django entende linkando so o type_id
 
     Gasto.objects.create(value=value, description=description, date_paid=date_paid, is_paid=is_paid, type_id=type_id if type_id else None, user=request.user)
+
+    return redirect('finance:dashboard')
+
+@login_required(login_url="/finance/login/")
+def registrar_ganho(request):
+    if request.method == "GET":
+        return redirect('finance:dashboard')
+    
+    value = request.POST.get('value')
+    description = request.POST.get('description')
+    date_paid = request.POST.get('date_paid')
+    is_paid = request.POST.get('is_paid') == 'True' # Recebe ou True ou Null do html
+
+    type_id = request.POST.get('type') # Do html recebe so o id, ai o Django entende linkando so o type_id
+
+    Ganho.objects.create(value=value, description=description, date_paid=date_paid, is_paid=is_paid, type_id=type_id if type_id else None, user=request.user)
 
     return redirect('finance:dashboard')
