964289f9bd009047cea58b552464e1d23925ebcb
Felpo0132 <felipesiilvaa2006@gmail.com>
2026-07-14 21:42:10 -0300

    Refatorando models

--- finance/models.py
+++ finance/models.py
@@ -10,43 +10,40 @@ class ProfileUser(AbstractUser):
     USERNAME_FIELD = 'email' # Django assim entende que o email sera o diferencial
     REQUIRED_FIELDS = ['username'] # Aqui ainda mantem o username importante, mas nao como campo de login
 
-class TipoGasto(models.Model):
+class Account(models.Model):
     name = models.CharField(max_length=255)
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return self.name
-
-class Gasto(models.Model):
-    value = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-    description = models.CharField(max_length=255)
-    date_paid = models.DateField(null=True, blank=True)
-    is_paid = models.BooleanField(default=True)
-
-    type = models.ForeignKey(TipoGasto, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # safer, since the login is based on a custom field (email)
 
     def __str__(self):
-        return self.description
-
-class TipoGanho(models.Model):
-    name = models.CharField(max_length=255)
-
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    
-    def __str__(self):
         return self.name
     
+class Category(models.Model):
+    class Type(models.TextChoices):
+        GASTO = "G", "Gasto"
+        RECEITA = "R", "Receita"
 
-class Ganho(models.Model):
-    value = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-    description = models.CharField(max_length=255)
-    date_paid = models.DateField(null=True, blank=True)
-    is_paid = models.BooleanField(default=True)
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=1, choices=Type.choices) # Recive 1 char and return the choice, G -> Gasto
 
-    type = models.ForeignKey(TipoGanho, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # safer, since the login is based on a custom field (email)
 
     def __str__(self):
-        return self.description
+        return self.name
+
+class Transaction(models.Model):
+    class Type(models.TextChoices):
+        GASTO = "G", "Gasto"
+        RECEITA = "R", "Receita"
+
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=1, choices=Type.choices)
+    date = models.DateField(null=True, blank=True)
+    is_paid = models.BooleanField(default=True)
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    conta = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
