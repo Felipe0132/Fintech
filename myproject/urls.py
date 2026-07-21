243722d5492c9d3b7e55d78a93902b9c3981b260
Felpo0132 <felipesiilvaa2006@gmail.com>
2026-07-21 09:19:47 -0300

    Caminho padrao

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('finance/')), # Caminho padrao inicial
+
     path('admin/', admin.site.urls),
     path('finance/', include('finance.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
